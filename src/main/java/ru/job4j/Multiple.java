f8a28f5795d43f816981e3f4f6e492a67aa03755
Lambere <krutysh.super@yandex.ru>
2022-03-22 12:46:18 +0300

    Multiple.java

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,6 +1,6 @@
 package ru.job4j;
 
-public class Multiple{
+ public class Multiple{
  public static void main(String[] args){
   System.out.println("1 * 2 = 2");
   System.out.println("1 * 3 = 3");
@@ -11,4 +11,4 @@ public class Multiple{
   System.out.println("1 * 8 = 8");
   System.out.println("1 * 9 = 9");
  }
-}
+ }
