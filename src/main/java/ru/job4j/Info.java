7ad2f0b64933a83e8618f23c752cf62b71aa1853
Lambere <krutysh.super@yandex.ru>
2022-03-23 17:32:54 +0300

    Метод main класса ru.job4j.Info.

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,8 +1,7 @@
 package ru.job4j;
 
 public class Info {
-    public static void maim(String[] args) {
-       System.out.print("23.03.2022");
-
+    public static void main(String[] args) {
+        System.out.print("23.03.2022");
     }
 }
