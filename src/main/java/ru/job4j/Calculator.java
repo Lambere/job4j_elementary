36eec8aeb266096ff7c0f7829d7edd5642cf24e0
Lambere <krutysh.super@yandex.ru>
2022-03-24 15:34:10 +0300

    Calculator

---
+++ src/main/java/ru/job4j/Calculator.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int two = 2;
+        int one = 1;
+        int six = 6;
+        int five = 5;
+        int four = 4;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
+    }
+}
