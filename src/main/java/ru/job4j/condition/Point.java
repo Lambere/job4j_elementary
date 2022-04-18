b60a79b3fb7239e0c423866599c9a3af3ff01195
Lambere <krutysh.super@yandex.ru>
2022-04-18 21:31:21 +0300

    Point

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -3,19 +3,12 @@ package ru.job4j.condition;
 public class Point {
     public static double distance(double x1, double y1, double x2, double y2) {
 
-        double x = x2 - x1;
-        double y = y2 - y1;
-        double rsl = -1;
-        double first = Math.sqrt(x);
-        double second = Math.sqrt(y);
-        double a = first + second;
-        double notNotFirst = Math.sqrt(a);
-
+        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow(y2 - y1, 2));
         return rsl;
     }
 
     public static void main(String[]args) {
-        double result = Point.distance(0,0,2,0);
+        double result = Point.distance(0, 0, 2, 0);
       System.out.print("    result (0,0) to (2, 0)  " + result);
 
    }
