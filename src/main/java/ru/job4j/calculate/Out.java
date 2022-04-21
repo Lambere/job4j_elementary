0f8a0bb15123725f1f14f59931b3232499c3ca4f
Lambere <krutysh.super@yandex.ru>
2022-04-21 21:38:30 +0300

    9.1. String

--- src/main/java/ru/job4j/calculate/Out.java
+++ src/main/java/ru/job4j/calculate/Out.java
@@ -2,7 +2,7 @@ package ru.job4j.calculate;
 
 public class Out {
     public static void main(String[] args) {
-        String idea = "I like Java! ";
+        String idea = "I like Java!";
         System.out.println(idea);
         idea = idea + "But i am a newbie  ";
         int year = 2022;
